ec9c4252dcdf050724f629fbb4fc255eebc180ce
Shubham Mule <shubhambmule@gmail.com>
2021-12-17 19:44:22 +0530

    [Shubham]Added: Basic list program

--- List.py
+++ List.py
@@ -1,21 +1,6 @@
-"""
-age =int(input("Enter your age"))
 
-if age<18:
-    print("you cant drive")
-elif age>99:
-    print("This age is not allowed for drive")
-elif age>18:
-    print("You can drive")
-
-else :
-    print("You have attend physically")
-
-"""
 list1=("shubham","Onkar","Adya","Bunty")
 
-print(list1)
-list1.append("ss")
-
-
+for item in list1:
+    print (item)
 
